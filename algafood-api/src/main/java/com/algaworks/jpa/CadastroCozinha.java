6c0601d85b3f2a2227cfc45ddfd0e03c4d03fc06
clara2021git <gisele.claracosta2020@gmail.com>
2021-01-27 17:14:44 -0200

    aulas curso

---
+++ algafood-api/src/main/java/com/algaworks/jpa/CadastroCozinha.java
@@ -0,0 +1,26 @@
+package com.algaworks.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	
+	public List<Cozinha> listar() {
+		return manager.createQuery("from Cozinha", Cozinha.class)
+				.getResultList();
+	}
+
+}
---
+++ algafood-api/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into cozinha (nome) values ( 'Tailandesa');
+insert into cozinha (nome) values ( 'indiana');
+insert into cozinha (nome) values ( 'japonesa');
+insert into cozinha (nome) values ( 'baiana');
